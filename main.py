f057525d4fe138bb68d4d413552a3d0548947cb2
Ricardo Antonio Gomez Villalobos <bardack134@gmail.com>
2024-02-05 06:42:50 +0900

    end

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
-
-
-
 # Abre el archivo starting_letter.txt en modo de lectura
 with open("starting_letter.txt", "r") as starting_letter_file:
     # Lee el contenido del archivo y lo almacena en una variable
