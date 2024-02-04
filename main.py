5f4ab13542fe518b62259e0b0b533e5ff18fae20
Ricardo Antonio Gomez Villalobos <bardack134@gmail.com>
2024-02-05 06:28:31 +0900

    end

---
+++ main.py
@@ -0,0 +1,37 @@
+#TODO: Create a letter using starting_letter.txt 
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+    
+#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+
+
+# Abre el archivo starting_letter.txt en modo de lectura
+with open("starting_letter.txt", "r") as starting_letter_file:
+    # Lee el contenido del archivo y lo almacena en una variable
+    starting_letter = starting_letter_file.read()
+
+# Abre el archivo invited_names.txt en modo de lectura
+with open("invited_names.txt", "r") as names_file:
+    # Inicializa una lista vacía para almacenar los nombres
+    list_of_names = []
+    # Itera sobre cada línea del archivo
+    for line in names_file:
+        # Agrega cada nombre a la lista list_of_names
+        list_of_names.append(line)
+
+# Elimina los saltos de línea al final de cada nombre y guarda los nombres en una nueva lista
+new_list_of_names = []
+for name in list_of_names:
+    new_list_of_names.append(name.strip())
+    
+    
+#TODO:Escribir los nombres extraidos en el archivo starting_letters    
+for name in new_list_of_names:
+    # Abre un archivo con el nombre correspondiente a cada nombre en la lista
+    with open(f"{name}.txt", "w") as letter:
+        # Escribe una carta personalizada para cada nombre, reemplazando el marcador de posición [name] con el nombre real
+        letter.write(starting_letter.replace("[name]", name))
